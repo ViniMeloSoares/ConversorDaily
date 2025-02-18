412ae89cdead62fc3f1c12cc6bc8c701e760ebdd
ViniMeloSoares <vinimelosoares@gmail.com>
2025-02-18 01:28:19 -0300

    spinners arrays

--- app/src/main/java/com/example/conversordaily/Result_Activity.kt
+++ app/src/main/java/com/example/conversordaily/Result_Activity.kt
@@ -1,6 +1,7 @@
 package com.example.conversordaily
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,33 @@ class Result_Activity : AppCompatActivity() {
         binding = ActivityResultBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val adapter_peso = ArrayAdapter.createFromResource(
+            this,
+            R.array.Peso,
+            android.R.layout.simple_spinner_item
+        )
+        adapter_peso.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spPesoEntrada.adapter = adapter_peso
+        binding.spPesoSaida.adapter = adapter_peso
+
+        val adapter_distancia = ArrayAdapter.createFromResource(
+            this,
+            R.array.distancia,
+            android.R.layout.simple_spinner_item
+        )
+        adapter_distancia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spDistanciaEntrada.adapter = adapter_distancia
+        binding.spDistanciaSaida.adapter = adapter_distancia
+
+        val adapter_volume = ArrayAdapter.createFromResource(
+            this,
+            R.array.Volume,
+            android.R.layout.simple_spinner_item
+        )
+        adapter_volume.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spVolumeEntrada.adapter = adapter_volume
+        binding.spVolumeSaida.adapter = adapter_volume
 
     }
 }
